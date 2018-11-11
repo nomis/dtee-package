928d7b2d1d9dfba7557b269b95df1401b7e72469
Simon Arlott <sa.me.uk>
2018-11-11 15:57:02 +0000

    Save storage space by symlinking deb .orig files

--- upload-deb.sh
+++ upload-deb.sh
@@ -13,6 +13,7 @@ if [ ! -e "$filename" ]; then
 	exit 1
 fi
 cmp "$filename" "source/$JFROG_PKG-$PKG_VER.tar.gz" || exit 1
+ln -sf "../../source/$JFROG_PKG-$PKG_VER.tar.gz" "deb/$DEB_DIR/${JFROG_PKG}_$PKG_VER.orig.tar.gz" || exit 1
 chmod a-w "$filename"
 
 for file in .dsc .debian.tar.xz; do
