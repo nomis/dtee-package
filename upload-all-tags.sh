63a0252e61537b302e1af7a00d1361f72929649e
Simon Arlott <sa.me.uk>
2018-11-11 21:58:56 +0000

    Upload RPM files

--- upload-all-tags.sh
+++ upload-all-tags.sh
@@ -12,7 +12,13 @@ while read -r tag; do
 	echo
 	echo "Debian: $tag"
 	bash upload-deb.sh "$tag" amd64 </dev/null || exit 1
-done < <(git tag | grep -E -e ^debian -e ^ubuntu | sort -n)
+done < <(git tag | grep -E -e ^debian- -e ^ubuntu- | sort -n)
+
+while read -r tag; do
+	echo
+	echo "RedHat: $tag"
+	bash upload-rpm.sh "$tag" x86_64 </dev/null || exit 1
+done < <(git tag | grep -E -e ^rhel- -e ^fedora- | sort -n)
 
 echo
 echo OK
